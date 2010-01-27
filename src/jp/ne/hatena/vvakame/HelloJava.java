d2b996b69e723cd4133e42c26e6296610026ffc7
vvakame <vvakame+dev@gmail.com>
2010-01-27 21:47:18 +0900

    ScalaとJavaを相互に行ったり来たりできるようにした

--- src/jp/ne/hatena/vvakame/HelloJava.java
+++ src/jp/ne/hatena/vvakame/HelloJava.java
@@ -4,29 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class HelloJava extends Activity implements OnClickListener {
+public class HelloJava extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.hello_java);
 
-		((Button) findViewById(R.id.changeActivityButton))
-				.setOnClickListener(this);
-	}
-
-	@Override
-	public void onClick(View v) {
-		Intent intent = null;
-		switch (v.getId()) {
-		case R.id.changeActivityButton:
-			intent = new Intent(this, HelloScala.class);
-			startActivity(intent);
-			break;
-		default:
-			break;
-		}
+		Button changeButton = (Button) findViewById(R.id.changeActivityButton);
+		changeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(HelloJava.this, HelloScala.class));
+			}
+		});
 	}
 }
--- src/jp/ne/hatena/vvakame/HelloScala.scala
+++ src/jp/ne/hatena/vvakame/HelloScala.scala
@@ -4,14 +4,25 @@ import _root_.android.app.Activity
 import _root_.android.content.Intent
 import _root_.android.os.Bundle
 import _root_.android.view.View
-import _root_.android.view.View.OnClickListener
 import _root_.android.widget.Button
 
 class HelloScala extends Activity {
-  override def onCreate(savedInstanceState: Bundle) = {
+  import android.view.View.OnClickListener
+
+  override def onCreate(savedInstanceState: Bundle):Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.hello_scala)
 
-    startActivity(new Intent(this, classOf[HelloJava]))
+    findViewById(R.id.changeActivityButton).asInstanceOf[Button]
+      .setOnClickListener(()=>{
+        startActivity(new Intent(this, classOf[HelloJava]))
+      })
+  }
+
+  implicit def funcOnClick(f:View => Unit): OnClickListener = {
+    new OnClickListener(){ def onClick(v: View) = f.apply(v) }
+  }
+  implicit def funcOnClick0(f:() => Unit): OnClickListener = {
+    new OnClickListener() { def onClick(v: View) = f.apply }
   }
 }
